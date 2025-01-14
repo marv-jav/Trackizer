f799230cb36feaeff83f6ddb33e8c42c277471cb
marv-javv <marvellous.ayoola@oceangrsmith.com>
2025-01-14 13:09:46 +0100

    Refreshed Project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.codestudio.trackizer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.codestudio.trackizer"
